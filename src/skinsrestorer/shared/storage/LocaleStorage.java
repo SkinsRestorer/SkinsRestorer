4d89cc29aa77cddb0cf5265ddb14b516f1a88e24
MaRoTheTroLLeR <marooohd@gmail.com>
2016-02-15 16:59:32 +0200

    Added colors and some new messages.

--- src/skinsrestorer/shared/storage/LocaleStorage.java
+++ src/skinsrestorer/shared/storage/LocaleStorage.java
@@ -39,26 +39,28 @@ public class LocaleStorage {
 		}
 	}
 
-	public String PLAYER_SKIN_CHANGE_SKIN_DATA_CLEARED = "Your skin data has been removed";
-	public String PLAYER_SKIN_CHANGE_COOLDOWN = "You can change your skin only once per 10 minutes";
-	public String PLAYER_SKIN_CHANGE_SUCCESS = "Your skin has been updated.";
-	public String PLAYER_SKIN_CHANGE_FAILED = "Skin fetch failed: ";
+	public String PLAYER_SKIN_CHANGE_SKIN_DATA_CLEARED = "&9Your skin data has been removed";
+	public String PLAYER_SKIN_COOLDOWN = "&cYou can change your skin only once per %s seconds";
+	public String PLAYER_SKIN_CHANGE_SUCCESS = "&9Your skin has been updated.";
+	public String PLAYER_SKIN_CHANGE_FAILED = "&4Skin fetch failed: ";
+	public String DO_YOU_WANT_SKIN = "&9Click me if you want your skin.";
 	
-	public String PLAYER_HAS_NO_PERMISSION = "You don't have permission to do this!";
+	public String PLAYER_HAS_NO_PERMISSION = "&cYou don't have permission to do this!";
 
-	public String SKIN_FETCH_FAILED_NO_PREMIUM_PLAYER = "Can't find a valid premium player with that name";
-	public String SKIN_FETCH_FAILED_NO_SKIN_DATA = "No skin data found for player with that name";
-	public String SKIN_FETCH_FAILED_PARSE_FAILED = "Can't decode skin data";
-	public String SKIN_FETCH_FAILED_RATE_LIMITED = "Rate limited";
-	public String SKIN_FETCH_FAILED_ERROR = "An error has occured";
-	public String SKIN_DATA_DROPPED = "Skin data for player %player dropped";
-	public String SKIN_DATA_SAVED = "Skin data saved successfully.";
-	public String SKIN_DATA_UPDATED = "Skin data updated";
-	public String SKIN_FETCH_FAILED = "Skin fetch failed: ";
-	public String MCAPI_FAILED_ERROR = "Tried to use mcapi.ca, but it failed.";
+	public String SKIN_FETCH_FAILED_NO_PREMIUM_PLAYER = "&cCan't find a valid premium player with that name";
+	public String SKIN_FETCH_FAILED_NO_SKIN_DATA = "&cNo skin data found for player with that name";
+	public String SKIN_FETCH_FAILED_PARSE_FAILED = "&cCan't decode skin data";
+	public String SKIN_FETCH_FAILED_RATE_LIMITED = "&cRate limited";
+	public String SKIN_FETCH_FAILED_MCAPI_OVERLOAD = "&cMcapi is overloaded.";
+	public String SKIN_FETCH_FAILED_ERROR = "&cAn error has occured";
+	public String SKIN_DATA_DROPPED = "&9Skin data for player %player dropped";
+	public String SKIN_DATA_SAVED = "&9Skin data saved successfully.";
+	public String SKIN_DATA_UPDATED = "&9Skin data updated";
+	public String SKIN_FETCH_FAILED = "&4Skin fetch failed: ";
+	public String MCAPI_FAILED_ERROR = "&cTried to use mcapi.ca, but it failed.";
 	public String TRYING_TO_USE_NCAPI = "[SkinsRestorer] Getting skin from Mojang failed. Using mcapi.ca!";
 	
-	public String USE_SKIN_HELP = "Use '/skin help' for help.";
+	public String USE_SKIN_HELP = "&9Use '/skin help' for help.";
 	public String PLAYER_HELP = "&8]&7&m-------------&r&8[ &9SkinsRestorer Help &8]&7&m-------------*r&8[\n&9/skin set <skinname> &9-&a Sets your skin.\n&9/skin clear &9-&a Clears your skin.";
 	
 	public String ADMIN_USE_SKIN_HELP = "&9Use '/skinsrestorer help' for help.";
