930f24832505fec8a5d5a6c085da4b660af33c5a
Th3Tr0LLeR <marooohd@gmail.com>
2016-03-25 01:04:54 +0200

    It should show the real error now.

--- src/skinsrestorer/shared/utils/MojangAPI.java
+++ src/skinsrestorer/shared/utils/MojangAPI.java
@@ -100,7 +100,7 @@ public class MojangAPI {
 			if (future.get() != null)
 				profile = future.get();
 		} catch (Exception e) {
-			throw new SkinFetchFailedException(SkinFetchFailedException.Reason.NO_PREMIUM_PLAYER);
+			throw new SkinFetchFailedException(e);
 		}
 
 		return profile;
