3aed9cde46322991aabee8f4ef1a2d5a97b6a3fe
Th3Tr0LLeR <marooohd@gmail.com>
2016-03-29 23:56:30 +0300

    Remove unused methods from the final jar

--- src/skinsrestorer/shared/utils/DataFiles.java
+++ src/skinsrestorer/shared/utils/DataFiles.java
@@ -10,11 +10,9 @@ import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
 
 public class DataFiles {
 	private File file;
@@ -128,8 +126,9 @@ public class DataFiles {
 		}
 		
 	}
- 
-	public void rename(String path, String pathName) {
+
+	public Object get(String path) {
+		String value = null;
 		int counter = 1;
 		int lineNumber = 1;
 		section(counter, path);
@@ -138,17 +137,287 @@ public class DataFiles {
 			if (line.startsWith(tab(counter)) || isCommented(line)) {
 				if (line.startsWith(sec.get(counter))) {
 					if (counter == sec.size()) {
-						content.put(lineNumber, tab(counter) + pathName);
+						value = line.replace(sec.get(counter) + " ", "");
 						break;
 					}
 					counter++;
 				}
 			} else {
-				throw new NullPointerException();
+				break;
 			}
 			lineNumber++;
 		}
+		return value;
 	}
+
+	public String getContents() {
+		String value = "";
+		for (int x = 1; x <= content.size(); x++) {
+			value = value + content.get(x) + "\n";
+		}
+		return value;
+	}
+
+	public boolean pathExists(String path) {
+		int counter = 1;
+		int lineNumber = 1;
+		while (content.get(lineNumber) != null && counter <= sec.size()) {
+			String line = content.get(lineNumber);
+			if (line.startsWith(tab(counter)) || isCommented(line)) {
+				if (line.startsWith(sec.get(counter))) {
+					if (counter == sec.size()) {
+						return true;
+					}
+					counter++;
+				}
+			} else {
+				break;
+			}
+			lineNumber++;
+		}
+		return false;
+	}
+
+	public List<?> getList(String path) {
+		listings.clear();
+		int counter = 1;
+		int lineNumber = 1;
+		section(counter, path);
+		String line = null;
+		while (content.get(lineNumber) != null) {
+			line = content.get(lineNumber);
+			if (line.startsWith(tab(counter))) {
+				if (line.startsWith(sec.get(counter))) {
+					if (counter == sec.size()) {
+						lineNumber++;
+						break;
+					}
+					counter++;
+				}
+			}
+			lineNumber++;
+		}
+		line = content.get(lineNumber);
+		while (line != null && (line.startsWith(tab(counter) + "- ") || isCommented(line))) {
+			if (line.startsWith(tab(counter) + "- "))
+				listings.add(line.trim().replaceFirst("- ", ""));
+			lineNumber++;
+			line = content.get(lineNumber);
+		}
+		return listings;
+	}
+
+	public List<String> getStringList(String path) {
+		List<String> list = new ArrayList<String>();
+		int counter = 0;
+		getList(path);
+		while (counter < listings.size()) {
+			list.add(listings.get(counter).toString());
+			counter++;
+		}
+		return list;
+	}
+
+	public String getString(String path) {
+		if (get(path) != null) {
+			return get(path).toString();
+		}
+		return null;
+	}
+
+	public boolean getBoolean(String path) {
+		if (path == null){
+			System.err.println("Path is null, Returning false");
+			return false;
+		}
+		return Boolean.parseBoolean(get(path).toString().trim());
+	}
+
+	public int getInt(String path) {
+		return Integer.parseInt(get(path).toString().trim());
+	}
+ 
+	public DataFiles copyDefaults(InputStream is, boolean overWrite) {
+		if (overWrite || !file.exists() || isEmpty()) {
+			if (is == null) {
+				System.out.println("[Warning] " + file.getName() + "'s .jar file have been modified!");
+				System.out.println("[Warning] Could not generate " + file.getName() + "!");
+				System.out.println("[Warning] Please stop and restart the server completely!");
+				return this;
+			}
+			try {
+				Files.copy(is, file.getAbsoluteFile().toPath(), StandardCopyOption.REPLACE_EXISTING);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return this;
+	}
+
+	public boolean isEmpty() {
+		Scanner input;
+		try {
+			input = new Scanner(file);
+			if (input.hasNextLine()) {
+				input.close();
+				return false;
+			}
+			input.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return true;
+	}
+
+	public void save() {
+		try {
+			FileWriter fw = new FileWriter(file, false);
+			for (int x = 1; x <= content.size(); x++) {
+				fw.write(content.get(x) + "\n");
+			}
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void reload() {
+		try {
+			File direc = new File(file.getPath().replace(file.getName(), ""));
+			if (!direc.exists())
+				direc.mkdirs();
+			if (!file.exists())
+				file.createNewFile();
+			content.clear();
+			int lineNumber = 0;
+			Scanner input = new Scanner(file);
+			while (input.hasNextLine()) {
+				String line = input.nextLine();
+				lineNumber++;
+				content.put(lineNumber, line);
+			}
+			input.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void delete() {
+		file.delete();
+	}
+
+	private boolean isCommented(String line) {
+		if (line.trim().startsWith("#") || line.trim().isEmpty())
+			return true;
+		return false;
+	}
+	
+	private boolean isListing(String line) {
+		if (line.trim().startsWith("- "))
+			return true;
+		return false;
+	}
+ 
+	private void section(int counter, String path) {
+		sec.clear();
+		if (path.isEmpty()) return;
+		String[] pathx = path.split("\\.");
+		for (String x : pathx) {
+			x = x + ":";
+			sec.put(counter, tab(counter) + x);
+			counter++;
+		}
+	}
+
+	private String tab(int tabNumber) {
+		String tab = "";
+		for (int x = tabNumber; x > 1; x--) {
+			tab = tab + "  ";
+		}
+		return tab;
+	}
+
+	public File getFile() {
+		return file;
+	}
+    ///////////////////////////////// Unused Methods //////////////////////////////////
+    /* 
+	public static Set<DataFiles> getFolderContents(String folderLoc) {
+		Set<DataFiles> files = new HashSet<>();
+		File file = new File(folderLoc);
+		if (!file.exists())
+			file.mkdirs();
+		if (file.isDirectory()) {
+			for (File f : file.listFiles()) {
+				files.add(new DataFiles(f));
+			}
+			return files;
+		}
+		return null;
+	}
+	
+	public float getFloat(String path) {
+		return Float.parseFloat(get(path).toString().trim());
+	}
+	
+	public long getLong(String path) {
+		return Long.parseLong(get(path).toString().trim());
+	}
+	
+	public List<Double> getDoubleList(String path) {
+		List<Double> list = new ArrayList<Double>();
+		getList(path);
+		int counter = 0;
+		while (counter < listings.size()) {
+			list.add(Double.parseDouble(listings.get(counter).toString().trim()));
+			counter++;
+		}
+		return list;
+	}
+ 
+	public List<Boolean> getBooleanList(String path) {
+		List<Boolean> list = new ArrayList<Boolean>();
+		getList(path);
+		int counter = 0;
+		while (counter < listings.size()) {
+			list.add(Boolean.parseBoolean(listings.get(counter).toString().trim()));
+			counter++;
+		}
+		return list;
+	}
+	
+	public double getDouble(String path) {
+		return Double.parseDouble(get(path).toString().trim());
+	}
+
+    @Deprecated
+	public DataFiles copyDefaults(String pathToResource, boolean overWrite) {
+		InputStream is = (DataFiles.class.getClassLoader().getResourceAsStream(pathToResource));
+		return copyDefaults(is, overWrite);
+	}
+	
+	public List<Integer> getIntList(String path) {
+		List<Integer> list = new ArrayList<Integer>();
+		getList(path);
+		int counter = 0;
+		while (counter < listings.size()) {
+			list.add(Integer.parseInt(listings.get(counter).toString().trim()));
+			counter++;
+		}
+		return list;
+	}
+
+	public List<Long> getLongList(String path) {
+		List<Long> list = new ArrayList<Long>();
+		getList(path);
+		int counter = 0;
+		while (counter < listings.size()) {
+			list.add(Long.parseLong(listings.get(counter).toString().trim()));
+			counter++;
+		}
+		return list;
+	}
+	
 	
 	public int lineNumber(String path) {
 		int counter = 1;
@@ -274,30 +543,7 @@ public class DataFiles {
 		}
 		throw new NullPointerException();
 	}
-
-	public Object get(String path) {
-		String value = null;
-		int counter = 1;
-		int lineNumber = 1;
-		section(counter, path);
-		while (content.get(lineNumber) != null && counter <= sec.size()) {
-			String line = content.get(lineNumber);
-			if (line.startsWith(tab(counter)) || isCommented(line)) {
-				if (line.startsWith(sec.get(counter))) {
-					if (counter == sec.size()) {
-						value = line.replace(sec.get(counter) + " ", "");
-						break;
-					}
-					counter++;
-				}
-			} else {
-				break;
-			}
-			lineNumber++;
-		}
-		return value;
-	}
-
+	
 	public Set<String> getPaths(String path) {
 		Set<String> paths = new HashSet<>();
 		int lineNumber = 1;
@@ -327,271 +573,26 @@ public class DataFiles {
 		}
 		return paths;
 	}
-
-	public String getContents() {
-		String value = "";
-		for (int x = 1; x <= content.size(); x++) {
-			value = value + content.get(x) + "\n";
-		}
-		return value;
-	}
-
-	public boolean pathExists(String path) {
+	
+	public void rename(String path, String pathName) {
 		int counter = 1;
 		int lineNumber = 1;
+		section(counter, path);
 		while (content.get(lineNumber) != null && counter <= sec.size()) {
 			String line = content.get(lineNumber);
 			if (line.startsWith(tab(counter)) || isCommented(line)) {
 				if (line.startsWith(sec.get(counter))) {
 					if (counter == sec.size()) {
-						return true;
-					}
-					counter++;
-				}
-			} else {
-				break;
-			}
-			lineNumber++;
-		}
-		return false;
-	}
-
-	public List<?> getList(String path) {
-		listings.clear();
-		int counter = 1;
-		int lineNumber = 1;
-		section(counter, path);
-		String line = null;
-		while (content.get(lineNumber) != null) {
-			line = content.get(lineNumber);
-			if (line.startsWith(tab(counter))) {
-				if (line.startsWith(sec.get(counter))) {
-					if (counter == sec.size()) {
-						lineNumber++;
+						content.put(lineNumber, tab(counter) + pathName);
 						break;
 					}
 					counter++;
 				}
+			} else {
+				throw new NullPointerException();
 			}
 			lineNumber++;
 		}
-		line = content.get(lineNumber);
-		while (line != null && (line.startsWith(tab(counter) + "- ") || isCommented(line))) {
-			if (line.startsWith(tab(counter) + "- "))
-				listings.add(line.trim().replaceFirst("- ", ""));
-			lineNumber++;
-			line = content.get(lineNumber);
-		}
-		return listings;
 	}
-
-	public List<String> getStringList(String path) {
-		List<String> list = new ArrayList<String>();
-		int counter = 0;
-		getList(path);
-		while (counter < listings.size()) {
-			list.add(listings.get(counter).toString());
-			counter++;
-		}
-		return list;
-	}
-
-	public List<Integer> getIntList(String path) {
-		List<Integer> list = new ArrayList<Integer>();
-		getList(path);
-		int counter = 0;
-		while (counter < listings.size()) {
-			list.add(Integer.parseInt(listings.get(counter).toString().trim()));
-			counter++;
-		}
-		return list;
-	}
-
-	public List<Long> getLongList(String path) {
-		List<Long> list = new ArrayList<Long>();
-		getList(path);
-		int counter = 0;
-		while (counter < listings.size()) {
-			list.add(Long.parseLong(listings.get(counter).toString().trim()));
-			counter++;
-		}
-		return list;
-	}
-
-	public List<Double> getDoubleList(String path) {
-		List<Double> list = new ArrayList<Double>();
-		getList(path);
-		int counter = 0;
-		while (counter < listings.size()) {
-			list.add(Double.parseDouble(listings.get(counter).toString().trim()));
-			counter++;
-		}
-		return list;
-	}
- 
-	public List<Boolean> getBooleanList(String path) {
-		List<Boolean> list = new ArrayList<Boolean>();
-		getList(path);
-		int counter = 0;
-		while (counter < listings.size()) {
-			list.add(Boolean.parseBoolean(listings.get(counter).toString().trim()));
-			counter++;
-		}
-		return list;
-	}
-
-	public String getString(String path) {
-		if (get(path) != null) {
-			return get(path).toString();
-		}
-		return null;
-	}
-
-	public boolean getBoolean(String path) {
-		if (path == null){
-			System.err.println("Path is null, Returning false");
-			return false;
-		}
-		return Boolean.parseBoolean(get(path).toString().trim());
-	}
-
-	public int getInt(String path) {
-		return Integer.parseInt(get(path).toString().trim());
-	}
-
-	public long getLong(String path) {
-		return Long.parseLong(get(path).toString().trim());
-	}
- 
-	public double getDouble(String path) {
-		return Double.parseDouble(get(path).toString().trim());
-	}
- 
-	public float getFloat(String path) {
-		return Float.parseFloat(get(path).toString().trim());
-	}
-
-    @Deprecated
-	public DataFiles copyDefaults(String pathToResource, boolean overWrite) {
-		InputStream is = (DataFiles.class.getClassLoader().getResourceAsStream(pathToResource));
-		return copyDefaults(is, overWrite);
-	}
- 
-	public DataFiles copyDefaults(InputStream is, boolean overWrite) {
-		if (overWrite || !file.exists() || isEmpty()) {
-			if (is == null) {
-				System.out.println("[Warning] " + file.getName() + "'s .jar file have been modified!");
-				System.out.println("[Warning] Could not generate " + file.getName() + "!");
-				System.out.println("[Warning] Please stop and restart the server completely!");
-				return this;
-			}
-			try {
-				Files.copy(is, file.getAbsoluteFile().toPath(), StandardCopyOption.REPLACE_EXISTING);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return this;
-	}
-
-	public boolean isEmpty() {
-		Scanner input;
-		try {
-			input = new Scanner(file);
-			if (input.hasNextLine()) {
-				input.close();
-				return false;
-			}
-			input.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		return true;
-	}
-
-	public void save() {
-		try {
-			FileWriter fw = new FileWriter(file, false);
-			for (int x = 1; x <= content.size(); x++) {
-				fw.write(content.get(x) + "\n");
-			}
-			fw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void reload() {
-		try {
-			File direc = new File(file.getPath().replace(file.getName(), ""));
-			if (!direc.exists())
-				direc.mkdirs();
-			if (!file.exists())
-				file.createNewFile();
-			content.clear();
-			int lineNumber = 0;
-			Scanner input = new Scanner(file);
-			while (input.hasNextLine()) {
-				String line = input.nextLine();
-				lineNumber++;
-				content.put(lineNumber, line);
-			}
-			input.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void delete() {
-		file.delete();
-	}
-
-	private boolean isCommented(String line) {
-		if (line.trim().startsWith("#") || line.trim().isEmpty())
-			return true;
-		return false;
-	}
-	
-	private boolean isListing(String line) {
-		if (line.trim().startsWith("- "))
-			return true;
-		return false;
-	}
- 
-	private void section(int counter, String path) {
-		sec.clear();
-		if (path.isEmpty()) return;
-		String[] pathx = path.split("\\.");
-		for (String x : pathx) {
-			x = x + ":";
-			sec.put(counter, tab(counter) + x);
-			counter++;
-		}
-	}
-
-	private String tab(int tabNumber) {
-		String tab = "";
-		for (int x = tabNumber; x > 1; x--) {
-			tab = tab + "  ";
-		}
-		return tab;
-	}
-
-	public File getFile() {
-		return file;
-	}
-
-	public static Set<DataFiles> getFolderContents(String folderLoc) {
-		Set<DataFiles> files = new HashSet<>();
-		File file = new File(folderLoc);
-		if (!file.exists())
-			file.mkdirs();
-		if (file.isDirectory()) {
-			for (File f : file.listFiles()) {
-				files.add(new DataFiles(f));
-			}
-			return files;
-		}
-		return null;
-	}
+	*/
 }
