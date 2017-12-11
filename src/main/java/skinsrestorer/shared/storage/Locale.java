042ef4fd9ce4a7f0e77250e5d50a6d5659fa5885
xknat <kevin.natte@gmail.com>
2017-12-11 20:05:35 +0100

    align locale.java +marked 2 old
    see, making strings align looks better!

--- src/main/java/skinsrestorer/shared/storage/Locale.java
+++ src/main/java/skinsrestorer/shared/storage/Locale.java
@@ -7,26 +7,17 @@ import java.lang.reflect.Field;
 
 public class Locale {
 
-    public static String NOT_PLAYER = "&4Error&8: &cYou need to be a player!";
-    public static String NOT_ONLINE = "&4Error&8: &cPlayer is not online!";
-    public static String SKIN_COOLDOWN_NEW = "&4Error&8: &cYou can change your skin again in &e%s &cseconds.";
-    public static String SKIN_CHANGE_SUCCESS = "&2Your skin has been changed.";
-    public static String SKIN_CLEAR_SUCCESS = "&2Your skin has been cleared.";
-    public static String PLAYER_HAS_NO_PERMISSION = "&4Error&8: &cYou don't have permission to do this.";
-    public static String SKIN_DISABLED = "&4Error&8: &cThis skin is disabled by an administrator.";
-    public static String UNKNOWN_COMMAND = "&cUnknown command. Type \"/help\" for help.";
-    public static String ALT_API_FAILED = "&4Error&8: &cSkin Data API is overloaded, please try again later!";
-    public static String NOT_PREMIUM = "&4Error&8: &cPremium player with that name does not exist.";
-    public static String WAIT_A_MINUTE = "&4Error&8: &cPlease wait a minute before requesting that skin again. (Rate Limited)";
-    public static String GENERIC_ERROR = "&4Error&8: &cAn error occured while requesting skin data, please try again later!";
-    public static String SKIN_DATA_DROPPED = "&2Skin data for player %player dropped.";
-    public static String SKIN_DATA_SAVED = "&2Skin data saved successfully.";
     public static String SR_LINE = "&7&m----------------------------------------";
+//    public static String UNKNOWN_COMMAND = "&cUnknown command. Type \"/help\" for help."; #history books
+    public static String PLAYER_HAS_NO_PERMISSION = "&4Error&8: &cYou don't have permission to do this.";
     public static String HELP_PLAYER = "  &2&lSkinsRestorer &7- &f&lv%ver%"
             + "\n    &2/skin set <skinname> &7-&f Changes your skin."
             + "\n    &2/skin clear &7-&f Clears your skin.";
     public static String HELP_SR = "    &2/sr &7- &fDisplay admin commands.";
-    public static String ADMIN_SET_SKIN = "&2You set %player's skin.";
+    public static String NOT_PREMIUM = "&4Error&8: &cPremium player with that name does not exist.";
+    public static String SKIN_COOLDOWN_NEW = "&4Error&8: &cYou can change your skin again in &e%s &cseconds.";
+    public static String SKIN_CHANGE_SUCCESS = "&2Your skin has been changed.";
+    public static String SKIN_CLEAR_SUCCESS = "&2Your skin has been cleared.";
     public static String HELP_ADMIN = "  &2&lSkinsRestorer &7- &f&lv%ver% &c&lAdmin"
             + "\n\n    &2/sr config &7- &fhelp page for usefull in game config settings"
             + "\n    &2/sr set <player> <skin name> &7- &fChanges the skin of a player.."
@@ -34,6 +25,11 @@ public class Locale {
             + "\n    &2/sr reload &7- &fReloads the config and locale"
             + "\n    &2/sr props [player] &7- &fDisplays the players actual skin as properties"
             + "\n    &2/sr status &7- &fChecks if Mojang API is available.";
+    public static String ADMIN_SET_SKIN = "&2You set %player's skin.";
+    public static String NOT_ONLINE = "&4Error&8: &cPlayer is not online!";
+    public static String SKIN_DATA_DROPPED = "&2Skin data for player %player dropped.";
+//    public static String SKIN_DATA_SAVED = "&2Skin data saved successfully."; #history books
+    public static String RELOAD = "&2Config and Locale has been reloaded!";
     public static String HELP_CONFIG = "  &2&lSkinsRestorer &7- &c&lConfig"
             + "\n\n    &2/sr joinSkins <true/false> &7- &fToggles the skins on join."
             + "\n    &2/sr SkinWithoutPerm <true/false> &7- &fConfigures the DisabledSkins section."
@@ -41,10 +37,14 @@ public class Locale {
             + "\n    &2/sr skinCooldown <time> &7- &f/skin cooldown in minute(s)."
             + "\n    &2/sr defaultSkins <true/false/add [skin]> &7- &fConfigures the DefaultSkins section."
             + "\n    &2/sr updater <true/false> &7- &fToggles the updater";
-    public static String RELOAD = "&2Config and Locale has been reloaded!";
+    public static String SKIN_DISABLED = "&4Error&8: &cThis skin is disabled by an administrator.";
+    public static String ALT_API_FAILED = "&4Error&8: &cSkin Data API is overloaded, please try again later!";
     public static String NO_SKIN_DATA = "&4Error&8: &cNo skin data acquired! Does this player have a skin?";
     public static String STATUS_OK = "&aMojang API connection successful!";
+    public static String GENERIC_ERROR = "&4Error&8: &cAn error occurred while requesting skin data, please try again later!";
+    public static String WAIT_A_MINUTE = "&4Error&8: &cPlease wait a minute before requesting that skin again. (Rate Limited)";
     public static String OUTDATED = "&4You are running an outdated version of SkinsRestorer!\n&cPlease update to the latest version on Spigot: \n&ehttps://www.spigotmc.org/resources/skinsrestorer.2124/";
+    public static String NOT_PLAYER = "&4Error&8: &cYou need to be a player!";
     private static YamlConfig locale = new YamlConfig(
             "plugins" + File.separator + "SkinsRestorer" + File.separator + "", "msgs");
 
@@ -60,7 +60,7 @@ public class Locale {
                 f.set(null, C.c(locale.getString(f.getName(), f.get(null))));
             }
         } catch (Exception e) {
-            System.out.println("§e[§2SkinsRestorer§e] §cCan't read messages.yml! Try removing it and restarting.");
+            System.out.println("§e[§2SkinsRestorer§e] §cCan't read messages.yml! Try removing it and restart your mc server.");
         }
     }
 }
