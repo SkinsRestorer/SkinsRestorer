5e2c06bc83f7de1c5baeb9598fc21f10312839b3
McLive <mail@mclive.eu>
2018-04-02 15:51:50 +0200

    Remove unused imports

--- src/main/java/skinsrestorer/sponge/commands/SkinCommand.java
+++ src/main/java/skinsrestorer/sponge/commands/SkinCommand.java
@@ -1,21 +1,14 @@
 package skinsrestorer.sponge.commands;
 
-import com.flowpowered.math.vector.Vector3d;
-import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
 import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.entity.living.player.gamemode.GameModes;
-import org.spongepowered.api.entity.living.player.tab.TabList;
-import org.spongepowered.api.entity.living.player.tab.TabListEntry;
 import org.spongepowered.api.profile.property.ProfileProperty;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.text.format.TextColors;
-import org.spongepowered.api.world.Location;
-import org.spongepowered.api.world.World;
 import skinsrestorer.sponge.SkinsRestorer;
 import skinsrestorer.sponge.utils.MojangAPI;
 
