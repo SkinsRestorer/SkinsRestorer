6d418ff737e6c32f10a9eee4147e785c27cff901
TeKGameR950 <tekgamer950@gmail.com>
2018-05-15 21:18:01 +0200

    Update SkinApplier.java

--- src/main/java/skinsrestorer/sponge/utils/SkinApplier.java
+++ src/main/java/skinsrestorer/sponge/utils/SkinApplier.java
@@ -1,24 +1,23 @@
 package skinsrestorer.sponge.utils;
 
-import com.flowpowered.math.vector.Vector3d;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.tab.TabList;
 import org.spongepowered.api.entity.living.player.tab.TabListEntry;
-import org.spongepowered.api.item.inventory.entity.Hotbar;
 import org.spongepowered.api.text.Text;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
+import org.spongepowered.api.world.storage.WorldProperties;
+
+import com.flowpowered.math.vector.Vector3d;
+
 import skinsrestorer.sponge.SkinsRestorer;
 
 /**
  * Created by McLive on 02.04.2018.
  */
 public class SkinApplier {
-    private SkinsRestorer plugin;
-
     public SkinApplier(SkinsRestorer plugin) {
-        this.plugin = plugin;
     }
 
     public void updatePlayerSkin(Player p) {
