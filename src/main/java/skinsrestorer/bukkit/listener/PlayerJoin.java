123db4539baf321eedbd7dea1a07e319984feda2
McLive <mail@mclive.eu>
2020-03-17 16:11:13 +0100

    Switch to PlayerLoginEvent, maybe it helps with ghosts ¯\_(ツ)_/¯

--- src/main/java/skinsrestorer/bukkit/listener/PlayerJoin.java
+++ src/main/java/skinsrestorer/bukkit/listener/PlayerJoin.java
@@ -1,9 +1,11 @@
 package skinsrestorer.bukkit.listener;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import skinsrestorer.bukkit.SkinsRestorer;
 import skinsrestorer.shared.exception.SkinRequestException;
 import skinsrestorer.shared.storage.Config;
@@ -19,8 +21,14 @@ public class PlayerJoin implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerJoin(PlayerLoginEvent e) {
+        if (e.getResult() != PlayerLoginEvent.Result.ALLOWED) {
+            return;
+        }
+
+        Player p = e.getPlayer();
+
         Bukkit.getScheduler().runTaskAsynchronously(SkinsRestorer.getInstance(), () -> {
             try {
                 if (Config.DISABLE_ONJOIN_SKINS) {
@@ -30,14 +38,14 @@ public class PlayerJoin implements Listener {
                 }
 
                 // Don't change skin if player has no custom skin-name set and his username is invalid
-                if (plugin.getSkinStorage().getPlayerSkin(e.getPlayer().getName()) == null && !C.validUsername(e.getPlayer().getName())) {
-                    System.out.println("[SkinsRestorer] Not applying skin to " + e.getPlayer().getName() + " (invalid username).");
+                if (plugin.getSkinStorage().getPlayerSkin(p.getName()) == null && !C.validUsername(p.getName())) {
+                    System.out.println("[SkinsRestorer] Not applying skin to " + p.getName() + " (invalid username).");
                     return;
                 }
 
-                String skin = plugin.getSkinStorage().getDefaultSkinNameIfEnabled(e.getPlayer().getName());
+                String skin = plugin.getSkinStorage().getDefaultSkinNameIfEnabled(p.getName());
 
-                SkinsRestorer.getInstance().getFactory().applySkin(e.getPlayer(), plugin.getSkinStorage().getOrCreateSkinForPlayer(skin));
+                SkinsRestorer.getInstance().getFactory().applySkin(p, plugin.getSkinStorage().getOrCreateSkinForPlayer(skin));
             } catch (SkinRequestException ignored) {
             }
         });
