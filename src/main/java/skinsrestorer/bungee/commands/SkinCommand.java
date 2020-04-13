a9f84d8bc6259f29a9174d4bf8b7d9c5293ebcbc
xknat <kevin.natte@gmail.com>
2020-04-13 16:43:25 +0200

    fix skin null issue

--- src/main/java/skinsrestorer/bungee/commands/SkinCommand.java
+++ src/main/java/skinsrestorer/bungee/commands/SkinCommand.java
@@ -142,7 +142,7 @@ public class SkinCommand extends BaseCommand {
     // if save is false, we won't save the skin skin name
     // because default skin names shouldn't be saved as the users custom skin
     private boolean setSkin(CommandSender sender, ProxiedPlayer p, String skin, boolean save) {
-        if (!C.validUsername(skin) && !C.validUrl(skin)) {
+        if (skin.equalsIgnoreCase("null") || !C.validUsername(skin) && !C.validUrl(skin)) {
             sender.sendMessage(new TextComponent(Locale.INVALID_PLAYER.replace("%player", skin)));
             return false;
         }
