d1c5136ea7d34758173fe9170e8a85123472c55d
xknat <kevin.natte@gmail.com>
2020-04-16 15:35:13 +0200

    not the Â again...

--- src/main/java/skinsrestorer/shared/storage/Locale.java
+++ src/main/java/skinsrestorer/shared/storage/Locale.java
@@ -45,8 +45,8 @@ public class Locale {
     public static String WAIT_A_MINUTE = "&4Error&8: &cPlease wait a minute before requesting that skin again. (Rate Limited)";
 
     public static String MENU_OPEN = "&2Opening the skins menu...";
-    public static String NEXT_PAGE = "&a&lÂ»&7 Next Page&a&l Â»";
-    public static String PREVIOUS_PAGE = "&e&lÂ«&7 Previous Page&e&l Â«";
+    public static String NEXT_PAGE = "&a&l»&7 Next Page&a&l »";
+    public static String PREVIOUS_PAGE = "&e&l«&7 Previous Page&e&l «";
     public static String REMOVE_SKIN = "&c&l[ &7Remove Skin&c&l ]";
     public static String SELECT_SKIN = "&2Click to select this skin";
 
@@ -96,7 +96,7 @@ public class Locale {
                 f.set(null, parsed);
             }
         } catch (Exception e) {
-            System.out.println("Â§e[Â§2SkinsRestorerÂ§e] Â§cCan't read messages.yml! Try removing it and restart your server.");
+            System.out.println("§e[§2SkinsRestorer§e] §cCan't read messages.yml! Try removing it and restart your server.");
         }
     }
 }
