3da4e6594208fce5ebf4d05567409e2a8752fd85
xknat <kevin.natte@gmail.com>
2020-09-27 14:56:15 +0200

    Warning file should also use getDataFolder path

--- src/main/java/skinsrestorer/bukkit/SkinsRestorer.java
+++ src/main/java/skinsrestorer/bukkit/SkinsRestorer.java
@@ -379,7 +379,7 @@ public class SkinsRestorer extends JavaPlugin {
         }
 
         try {
-            File warning = new File("plugins" + File.separator + "SkinsRestorer" + File.separator + "Use bungee config for settings!");
+            File warning = new File(getDataFolder() + File.separator + "Use bungee config for settings!");
             warning.getParentFile().mkdirs();
             if (!warning.exists() && bungeeEnabled)
                 warning.createNewFile();
