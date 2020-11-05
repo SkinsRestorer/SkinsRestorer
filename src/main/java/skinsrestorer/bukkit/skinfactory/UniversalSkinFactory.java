108bd1297111e3366f21f5f9bdaa579f2a38099d
xknat <kevin.natte@gmail.com>
2020-11-06 00:40:32 +0100

    + config option with remoundplayer
    * we now check for "disablesdismoundplayer" & "enablesdismoundentities" override files.
    * fixed typo (sorry)
    * DISMOUNT_PLAYER_ON_UPDATE = true; on default.

--- src/main/java/skinsrestorer/bukkit/skinfactory/UniversalSkinFactory.java
+++ src/main/java/skinsrestorer/bukkit/skinfactory/UniversalSkinFactory.java
@@ -1,12 +1,11 @@
 package skinsrestorer.bukkit.skinfactory;
 
 import lombok.RequiredArgsConstructor;
-import skinsrestorer.shared.storage.Config;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
+import skinsrestorer.shared.storage.Config;
 
 import java.io.File;
 import java.util.function.Consumer;
@@ -16,32 +15,45 @@ public class UniversalSkinFactory extends SkinFactory {
     private final Plugin plugin;
     private final Consumer<Player> refresh = detectRefresh();
     public static final String NMS_VERSION = Bukkit.getServer().getClass().getPackage().getName().substring(23);
+    private Entity Vehicle;
+    boolean checkoptfilechecked;
+    boolean DisableDismoundPlayer;
+    boolean EnableDismountentities;
+
 
     @Override
     public void updateSkin(Player player) {
         if (!player.isOnline())
             return;
 
+        if (checkoptfilechecked)
+            this.checkoptfile();
+
         Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+            //dismounts & remounts a player on refreshing, which prevents desync caused by riding a horse, or plugins that allow sitting
+            Vehicle = player.getVehicle();
+            if ((Config.DISMOUNT_PLAYER_ON_UPDATE || !DisableDismoundPlayer) && Vehicle != null) {
 
-        	//dismounts a player before refreshing, which prevents desync caused by riding a horse, or plugins that allow sitting
-        	if (Config.DISMOUNT_PLAYER_ON_UPDATE && player.getVehicle() != null) {
-        		
-        		player.getVehicle().removePassenger(player);
 
-        	}
-        	
-        	//dismounts all entities riding the player, preventing desync from plugins that allow players to mount each other
-        	if (Config.DISMOUNT_PASSENGERS_ON_UPDATE && !player.getPassengers().isEmpty()) {
-        		
-        		for (Entity passenger : player.getPassengers()) {
-        		
-        			player.removePassenger(passenger);
-        			
-        		}
+                Vehicle.removePassenger(player);
+                Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+                    public void run() {
+                        Vehicle.addPassenger(player);
+                    }
+                }, 1);
 
-        	}
-        	
+            }
+
+            //dismounts all entities riding the player, preventing desync from plugins that allow players to mount each other
+            if ((Config.DISMOUNT_PASSENGERS_ON_UPDATE || EnableDismountentities) && !player.getPassengers().isEmpty()) {
+                for (Entity passenger : player.getPassengers()) {
+
+                    player.removePassenger(passenger);
+
+                }
+
+            }
+
             for (Player ps : Bukkit.getOnlinePlayers()) {
                 // Some older spigot versions only support hidePlayer(player)
                 try {
@@ -83,4 +95,16 @@ public class UniversalSkinFactory extends SkinFactory {
         // return new LegacySkinRefresher();
         return new OldSkinRefresher();
     }
+    private void checkoptfile() {
+        File FileDisableDismoundPlayer = new File("plugins" + File.separator + "SkinsRestorer" + File.separator + "disablesdismoundplayer");
+        File FileEnableDismountentities = new File("plugins" + File.separator + "SkinsRestorer" + File.separator + "enablesdismoundentities");
+        if (FileDisableDismoundPlayer.exists())
+            DisableDismoundPlayer = true;
+
+        if (FileEnableDismountentities.exists())
+            EnableDismountentities = true;
+
+        checkoptfilechecked = true;
+    }
+
 }
