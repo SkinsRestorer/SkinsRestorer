a6895b5f1b8019f6d3cd8d725b61e3b80f93550f
xknat <kevin.natte@gmail.com>
2021-04-20 23:21:55 +0200

    Add notes to C.java for intentional missing validations.

--- src/main/java/net/skinsrestorer/shared/utils/C.java
+++ src/main/java/net/skinsrestorer/shared/utils/C.java
@@ -26,6 +26,7 @@ import net.skinsrestorer.shared.storage.Config;
 import java.util.regex.Pattern;
 
 public class C {
+    //Note: Players who bought the game early in its development can have "-" in usernames.
     private static final Pattern namePattern = Pattern.compile("^[a-zA-Z0-9_\\-]+$");
     private static final Pattern urlPattern = Pattern.compile("^https?://.*");
 
@@ -46,6 +47,7 @@ public class C {
     }
 
     public static boolean validMojangUsername(String username) {
+        //Note: there are exceptions of players with under 3 characters, who bought the game early in its development.
         if (username.length() > 16)
             return false;
 
