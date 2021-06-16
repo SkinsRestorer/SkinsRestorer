651a357dbdeab31928dbba3e4a7314944cba4463
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-06-16 19:10:26 +0200

    Fix build

--- src/main/java/net/skinsrestorer/shared/serverinfo/ProtocolEnum.java
+++ src/main/java/net/skinsrestorer/shared/serverinfo/ProtocolEnum.java
@@ -36,13 +36,10 @@ public enum ProtocolEnum {
     MINECRAFT_1_17(755);
 
     ProtocolEnum(int protocolNumber) {
-
-
     }
 
     static {
         List<String> SUPPORTED_VERSIONS;
-        List<Integer> SUPPORTED_VERSION_IDS;
 
         ImmutableList.Builder<String> supportedVersions = ImmutableList.<String>builder().add(
                 "1.8.x",
@@ -56,38 +53,7 @@ public enum ProtocolEnum {
                 "1.16.x",
                 "1.17.x"
         );
-        ImmutableList.Builder<Integer> supportedVersionIds = ImmutableList.<Integer>builder().add(
-                ProtocolEnum .1_8,
-                ProtocolEnum .1_9,
-                ProtocolEnum .1_9_1,
-                ProtocolEnum .1_9_2,
-                ProtocolEnum .1_9_4,
-                ProtocolEnum .1_10,
-                ProtocolEnum .1_11,
-                ProtocolEnum .1_11_1,
-                ProtocolEnum .1_12,
-                ProtocolEnum .1_12_1,
-                ProtocolEnum .1_12_2,
-                ProtocolEnum .1_13,
-                ProtocolEnum .1_13_1,
-                ProtocolEnum .1_13_2,
-                ProtocolEnum .1_14,
-                ProtocolEnum .1_14_1,
-                ProtocolEnum .1_14_2,
-                ProtocolEnum .1_14_3,
-                ProtocolEnum .1_14_4,
-                ProtocolEnum .1_15,
-                ProtocolEnum .1_15_1,
-                ProtocolEnum .1_15_2,
-                ProtocolEnum .1_16,
-                ProtocolEnum .1_16_1,
-                ProtocolEnum .1_16_2,
-                ProtocolEnum .1_16_3,
-                ProtocolEnum .1_16_4,
-                ProtocolEnum .1_17
-        );
 
         SUPPORTED_VERSIONS = supportedVersions.build();
-        SUPPORTED_VERSION_IDS = supportedVersionIds.build();
     }
 }
