b2d21c1820f65907d57e780beb1a4ddeef84529e
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-07-03 21:58:54 +0200

    Small auto cleanup

--- src/main/java/net/skinsrestorer/shared/serverinfo/ServerVersion.java
+++ src/main/java/net/skinsrestorer/shared/serverinfo/ServerVersion.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -39,14 +39,6 @@ public class ServerVersion {
     private final int minor;
     private final int patch;
 
-    public String getFormatted() {
-        return major + "." + minor + "." + patch;
-    }
-
-    public boolean isNewer(ServerVersion version2) {
-        return VersionComparator.SEM_VER.isNewer(getFormatted(), version2.getFormatted());
-    }
-
     @Nullable
     public static String getNMSVersion() {
         try {
@@ -56,5 +48,13 @@ public class ServerVersion {
         } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | IllegalAccessException ignored) {
             return null;
         }
+    }
+
+    public String getFormatted() {
+        return major + "." + minor + "." + patch;
+    }
+
+    public boolean isNewer(ServerVersion version2) {
+        return VersionComparator.SEM_VER.isNewer(getFormatted(), version2.getFormatted());
     }
 }
