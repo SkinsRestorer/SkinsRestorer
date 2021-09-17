899ce060fd9f0ac84b7e4871af25c4779c346956
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-09-17 20:24:52 +0200

    Proper CI fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,14 @@
 rootProject.name = "skinsrestorer"
 
 plugins {
-    id("com.gradle.enterprise") version("3.7")
+    id("com.gradle.enterprise") version ("3.7")
 }
 
 gradleEnterprise {
     buildScan {
-        publishAlwaysIf(!System.getenv("CI").isNullOrEmpty())
+        if (!System.getenv("CI").isNullOrEmpty()) {
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
     }
 }
