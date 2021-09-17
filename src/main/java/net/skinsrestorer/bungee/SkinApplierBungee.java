d1a9d6e67ec7d2b002e9e889afd409bacaa363e5
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2021-09-17 21:10:39 +0200

    Improve bungeecord applier a little bit

--- src/main/java/net/skinsrestorer/bungee/SkinApplierBungee.java
+++ src/main/java/net/skinsrestorer/bungee/SkinApplierBungee.java
@@ -88,21 +88,22 @@ public class SkinApplierBungee {
 
     private void applyToHandler(InitialHandler handler, Property textures) throws ReflectionException {
         LoginResult profile = handler.getLoginProfile();
+        Property[] newProps = new Property[]{textures};
+
         if (profile == null) {
             try {
                 // NEW BUNGEECORD (id, name, property)
-                profile = new LoginResult(null, null, new Property[]{textures});
+                profile = new LoginResult(null, null, newProps);
             } catch (Exception error) {
                 // FALL BACK TO OLD (id, property)
                 profile = (LoginResult) ReflectionUtil.invokeConstructor(LoginResult.class,
                         new Class<?>[]{String.class, Property[].class},
-                        null, new Property[]{textures});
+                        null, newProps);
             }
+        } else {
+            profile.setProperties(newProps);
         }
 
-        Property[] newProps = new Property[]{textures};
-
-        profile.setProperties(newProps);
         ReflectionUtil.setObject(InitialHandler.class, handler, "loginProfile", profile);
     }
 
