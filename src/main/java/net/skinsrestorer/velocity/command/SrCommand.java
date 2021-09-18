924e165eb857652cfe66a61f07d88b759c32970c
xknat <kevin.natte@gmail.com>
2021-09-18 19:58:41 +0200

    Use lombok @Getter in ServiceCheckResponse

--- src/main/java/net/skinsrestorer/velocity/command/SrCommand.java
+++ src/main/java/net/skinsrestorer/velocity/command/SrCommand.java
@@ -80,15 +80,17 @@ public class SrCommand extends BaseCommand {
 
             ServiceChecker.ServiceCheckResponse response = checker.getResponse();
             List<String> results = response.getResults();
+            int workingUuidCount = response.getWorkingUUID().get();
+            int workingProfileCount = response.getWorkingProfile().get();
 
-            if (Config.DEBUG || !(response.getWorkingUUID() >= 1 && response.getWorkingProfile() >= 1))
+            if (Config.DEBUG || !(workingUuidCount>= 1 && workingProfileCount >= 1))
                 for (String result : results) {
                     if (Config.DEBUG || result.contains("✘"))
                         source.sendMessage(plugin.deserialize(result));
                 }
             source.sendMessage(plugin.deserialize("§7Working UUID API count: §6" + response.getWorkingUUID()));
             source.sendMessage(plugin.deserialize("§7Working Profile API count: §6" + response.getWorkingProfile()));
-            if (response.getWorkingUUID() >= 1 && response.getWorkingProfile() >= 1)
+            if (workingUuidCount>= 1 && workingProfileCount>= 1)
                 source.sendMessage(plugin.deserialize("§aThe plugin currently is in a working state."));
             else
                 source.sendMessage(plugin.deserialize("§cPlugin currently can't fetch new skins. \\n Connection is likely blocked because of firewall. \\n Please See http://skinsrestorer.net/firewall for more info"));
