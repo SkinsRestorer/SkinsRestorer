f2535607338793e58addda1ad5bf7dbfe56f08b9
xknat <kevin.natte@gmail.com>
2021-12-05 11:46:23 +0100

    Update dependencies (bungee)

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
     compileOnly("org.slf4j:slf4j-api:1.7.32")
 
     compileOnly("com.mojang:authlib:1.11")
-    compileOnly("net.md-5:bungeecord-proxy:1.17-R0.1-SNAPSHOT")
+    compileOnly("net.md-5:bungeecord-proxy:1.18-R0.1-SNAPSHOT")
     compileOnly("com.velocitypowered:velocity-api:3.0.1")
 }
 
