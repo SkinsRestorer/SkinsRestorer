4d25b5d6897696a2954ff12cf62096696d7454d6
xknat <kevin.natte@gmail.com>
2021-12-08 23:45:16 +0100

    add /sr setskinall
    - only console may execute this command
    - all skins will be set at once on async which could cause some massive performance impact for short time depending on player count.

--- velocity/src/main/java/net/skinsrestorer/velocity/SkinsRestorer.java
+++ velocity/src/main/java/net/skinsrestorer/velocity/SkinsRestorer.java
@@ -149,7 +149,7 @@ public class SkinsRestorer implements ISRPlugin {
         prepareACF(manager, srLogger);
 
         manager.registerCommand(new SkinCommand(this, srLogger));
-        manager.registerCommand(new SrCommand(this, srLogger));
+        manager.registerCommand(new SrCommand(this, srLogger, this.proxy));
     }
 
     private boolean initStorage() {
