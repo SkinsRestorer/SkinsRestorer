fbba4cf14316e7460f896f49aabd1ff2c7db3dec
xknat <kevin.natte@gmail.com>
2022-01-03 12:50:42 +0100

    add SkinsRestorerAPI setSkinData & genSkinUrl (#740)
    This pr add the following to the api:
    `public void setSkinData(String skinName, IProperty textures, @Nullable Long timestamp)`
    &
    `public IProperty genSkinUrl(String url, @Nullable String skinType) throws SkinRequestException`
    
    see https://github.com/SkinsRestorer/SkinsRestorerAPIExample/blob/master/src/main/java/net/skinsrestorer/SkinsRestorerAPIExample.java for example.
    
    Co-authored-by: AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>

--- bukkit/src/main/java/net/skinsrestorer/bukkit/commands/SrCommand.java
+++ bukkit/src/main/java/net/skinsrestorer/bukkit/commands/SrCommand.java
@@ -215,7 +215,7 @@ public class SrCommand extends BaseCommand {
             try {
                 if (C.validUrl(skinUrl)) {
                     plugin.getSkinStorage().setSkinData(name, plugin.getMineSkinAPI().genSkin(skinUrl, String.valueOf(skinType), null),
-                            Long.toString(System.currentTimeMillis() + (100L * 365 * 24 * 60 * 60 * 1000))); // "generate" and save skin for 100 years
+                            System.currentTimeMillis() + (100L * 365 * 24 * 60 * 60 * 1000)); // "generate" and save skin for 100 years
                     sender.sendMessage(Locale.SUCCESS_CREATE_SKIN.replace("%skin", name));
                 } else {
                     sender.sendMessage(Locale.ERROR_INVALID_URLSKIN);
