2079ba883aaf4c8e5c5cffbacb80c51eb0a3a00f
xknat <kevin.natte@gmail.com>
2022-01-07 12:13:07 +0100

    bump version to snapshot (#807)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 allprojects {
     group = "net.skinsrestorer"
-    version = "14.1.10"
+    version = "14.1.11-SNAPSHOT"
     description = "Ability to restore/change skins on servers! (Offline and Online Mode)"
 }
 
