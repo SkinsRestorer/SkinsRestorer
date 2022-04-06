0e8f276ca692dc8089d7dec5e05f23d19fc05ccb
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-04-06 17:12:33 +0200

    Fix deprecated warning and fixup dependencies
    Also adds more relocations. Should hopefully prevent collision with any other plugin.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -8,9 +8,7 @@ dependencies {
     implementation("com.google.code.gson:gson:2.9.0")
     implementation("com.google.guava:guava:31.1-jre")
     implementation("org.mariadb.jdbc:mariadb-java-client:3.0.4")
-    implementation("com.github.InventivetalentDev.Spiget-Update:bukkit:1.4.2-SNAPSHOT") {
-        exclude("org.bukkit", "bukkit")
-    }
+    api("com.github.InventivetalentDev.Spiget-Update:core:1.4.4-SNAPSHOT")
 
     implementation("org.fusesource.jansi:jansi:2.4.0")
 
