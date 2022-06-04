5695a78e8c4836d965a86162ce21c21b978f63ec
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-06-04 20:17:27 +0200

    Enhance build speed by dropping paperweight

--- buildSrc/src/main/kotlin/sr.mapping-logic.gradle.kts
+++ buildSrc/src/main/kotlin/sr.mapping-logic.gradle.kts
@@ -2,9 +2,23 @@ plugins {
     java
     id("sr.license-logic")
     id("sr.core-dependencies")
-    id("io.papermc.paperweight.userdev")
+    id("io.github.patrick.remapper")
 }
 
 dependencies.implementation(project(":mappings:shared"))
 
-tasks.named("build").get().dependsOn("reobfJar")
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.named("remap").get().dependsOn("jar")
+
+configurations {
+    create("reobfuscated") {
+        isCanBeResolved = false
+        isCanBeConsumed = true
+        outgoing.artifact(tasks.jar.get().archiveFile)
+    }
+}
