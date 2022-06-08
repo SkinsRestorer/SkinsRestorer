30c38c2626cd696b4ceae26175cedcb104d9c675
Dilan Olivera <dilanolivera12@gmail.com>
2022-06-08 04:34:06 -0300

    fix: Fixed compilation

--- bungee/build.gradle.kts
+++ bungee/build.gradle.kts
@@ -2,8 +2,8 @@ dependencies {
     implementation(projects.skinsrestorerApi)
     implementation(projects.skinsrestorerShared)
 
-    compileOnly("net.md-5:bungeecord-api:1.18-R0.1-SNAPSHOT")
-    compileOnly("net.md-5:bungeecord-proxy:1.18-R0.1-SNAPSHOT")
+    compileOnly("net.md-5:bungeecord-api:1.19-R0.1-SNAPSHOT")
+    compileOnly("net.md-5:bungeecord-proxy:1.19-R0.1-SNAPSHOT")
 
     implementation("org.bstats:bstats-bungeecord:3.0.0")
     implementation("co.aikar:acf-bungee:0.5.1-SNAPSHOT")
