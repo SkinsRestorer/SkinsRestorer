9406ea2caa2151ac00ea04ce2aad2ccc29553a38
xknat <kevin.natte@gmail.com>
2022-06-29 15:32:04 +0200

    Improve locale

--- shared/src/main/java/net/skinsrestorer/shared/storage/Locale.java
+++ shared/src/main/java/net/skinsrestorer/shared/storage/Locale.java
@@ -41,13 +41,14 @@ public class Locale {
     };
     public static String PREFIX = "&e[&2SkinsRestorer&e] ";
     public static String HELP_HELP_COMMAND = " [help]";
+    public static String HELP_SKIN_SET = "Changes your skin.";
+    public static String HELP_SKIN_SET_OTHER = "Sets the skin for a target player.";
+    public static String HELP_SKIN_SET_OTHER_URL = "Sets the skin from a url for a target player.";
     public static String HELP_SKIN_CLEAR = "Clears your skin.";
     public static String HELP_SKIN_CLEAR_OTHER = "Clears the skin of a target player.";
     public static String HELP_SKIN_UPDATE = "Updates your skin.";
     public static String HELP_SKIN_UPDATE_OTHER = "Updates the skin of a target player.";
-    public static String HELP_SKIN_SET = "Set the skin of another player.";
-    public static String HELP_SKIN_SET_OTHER = "Sets the skin for a target player.";
-    public static String HELP_SKIN_SET_OTHER_URL = "Set a skin by image_url.png link";
+
     public static String HELP_SR_RELOAD = "Reloads the configuration file.";
     public static String HELP_SR_STATUS = "Checks plugin needed API services.";
     public static String HELP_SR_DROP = "Removes players or skin data.";
