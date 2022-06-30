237cc00e2fc94dababf7159b0290efd1d3b2a43f
xknat <kevin.natte@gmail.com>
2022-06-30 16:12:01 +0200

    improve

--- shared/src/main/java/net/skinsrestorer/shared/storage/SkinStorage.java
+++ shared/src/main/java/net/skinsrestorer/shared/storage/SkinStorage.java
@@ -643,13 +643,13 @@ public class SkinStorage implements ISkinStorage {
 
     public boolean purgeOldSkins(int days) {
         long targetPurgeTimestamp = System.currentTimeMillis() - ((long) days * 86400 * 1000);
+
         if (Config.MYSQL_ENABLED) {
             // delete if name not start with " " and timestamp below targetPurgeTimestamp
             mysql.execute("DELETE FROM " + Config.MYSQL_SKIN_TABLE + " WHERE Nick NOT LIKE ' %' AND NOT " + Config.MYSQL_SKIN_TABLE + ".timestamp 0 AND " + Config.MYSQL_SKIN_TABLE + ".timestamp<=?", targetPurgeTimestamp);
             return true;
         } else {
             try (DirectoryStream<Path> stream = Files.newDirectoryStream(skinsFolder, "*.skin")) {
-
                 for (Path file : stream) {
                     try {
                         if (!Files.exists(file))
