94de1ab86e1da632d31740721168ffc2a751ca05
xknat <kevin.natte@gmail.com>
2022-09-02 15:58:48 +0200

    keep bungee proxy 1.18 for SkinApplierBungeeOld

--- multiver/bungee/propertyold/build.gradle.kts
+++ multiver/bungee/propertyold/build.gradle.kts
@@ -8,5 +8,5 @@ dependencies {
     implementation(projects.multiver.bungee.shared)
 
     compileOnly("net.md-5:bungeecord-api:1.19-R0.1-SNAPSHOT")
-    compileOnly("net.md-5:bungeecord-proxy:1.19-R0.1-SNAPSHOT")
+    compileOnly("net.md-5:bungeecord-proxy:1.18-R0.1-SNAPSHOT") // keep this version for SkinApplierBungeeOld
 }
