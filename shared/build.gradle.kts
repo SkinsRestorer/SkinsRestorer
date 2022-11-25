4768ce83ad39c4974bf88576fdfae9ea76f439fa
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-11-25 09:21:39 +0100

    Revert "Update dependency org.mariadb.jdbc:mariadb-java-client to v3.1.0 (#1108)"
    This reverts commit 80fa1e1bcd65392aead4235d20907499fbf4e6d2.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,7 +5,7 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
 
     implementation("com.google.code.gson:gson:2.10")
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.1.0")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.0.9")
     api("com.github.InventivetalentDev.Spiget-Update:core:1.4.6-SNAPSHOT")
 
     implementation("org.fusesource.jansi:jansi:2.4.0")
