46d964351df9755a66c2e0d1926357cab1bc6727
xknat <kevin.natte@gmail.com>
2022-12-12 15:30:57 +0100

    Back to velocity 3.1.1

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -3,8 +3,8 @@ dependencies {
     implementation(projects.skinsrestorerApi)
     implementation(projects.skinsrestorerShared)
 
-    compileOnly("com.velocitypowered:velocity-api:3.1.0")
-    annotationProcessor("com.velocitypowered:velocity-api:3.1.0")
+    compileOnly("com.velocitypowered:velocity-api:3.1.1")
+    annotationProcessor("com.velocitypowered:velocity-api:3.1.1")
 
     implementation("org.bstats:bstats-velocity:3.0.0")
     implementation("co.aikar:acf-velocity:0.5.1-SNAPSHOT")
