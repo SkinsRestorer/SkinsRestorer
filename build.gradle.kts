52ea36187672bb85b6a9323ef602e44f493cdc4d
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2022-12-23 19:05:46 +0100

    Bump to SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 allprojects {
     group = "net.skinsrestorer"
-    version = "14.2.4"
+    version = "14.2.5-SNAPSHOT"
     description = "Ability to restore/change skins on servers! (Offline and Online Mode)"
 }
 
