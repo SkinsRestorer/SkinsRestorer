9b19fe2b6f67b8ef484f6ff25849a56a7a7124c9
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-01-18 14:31:09 +0100

    Use mariadb java driver snapshot that fixes breaking errors

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -6,8 +6,8 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.1.1") {
-        exclude ("com.github.waffle", "waffle-jna")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.1.2-SNAPSHOT") {
+        exclude("com.github.waffle", "waffle-jna")
     }
 
     api("com.github.InventivetalentDev.Spiget-Update:core:1.4.6-SNAPSHOT")
