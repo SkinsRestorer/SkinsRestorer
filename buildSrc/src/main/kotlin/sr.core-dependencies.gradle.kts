51f0cbebc9c3c8ce2000efdd895e4fb9048b937f
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-02-25 21:11:05 +0100

    Cleanup

--- buildSrc/src/main/kotlin/sr.core-dependencies.gradle.kts
+++ buildSrc/src/main/kotlin/sr.core-dependencies.gradle.kts
@@ -2,16 +2,18 @@ plugins {
     java
 }
 
-dependencies.compileOnly("org.projectlombok:lombok:1.18.26")
-dependencies.annotationProcessor("org.projectlombok:lombok:1.18.26")
-dependencies.implementation("org.jetbrains:annotations:24.0.1")
-dependencies.compileOnly("com.github.spotbugs:spotbugs-annotations:4.7.3")
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+    implementation("org.jetbrains:annotations:24.0.1")
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.7.3")
 
-dependencies.testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-dependencies.testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-dependencies.testImplementation("org.mockito:mockito-core:4.8.0")
-dependencies.testImplementation("org.mockito:mockito-inline:4.8.0")
-dependencies.testImplementation("org.mockito:mockito-junit-jupiter:4.8.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("org.mockito:mockito-core:4.8.0")
+    testImplementation("org.mockito:mockito-inline:4.8.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:4.8.0")
+}
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
--- shared/src/test/java/ServicesTest.java
+++ shared/src/test/java/ServicesTest.java
@@ -12,11 +12,14 @@ import net.skinsrestorer.shared.utils.log.SRLogLevel;
 import net.skinsrestorer.shared.utils.log.SRLogger;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+@ExtendWith(MockitoExtension.class)
 public class ServicesTest {
     @Mock
     private SRPlatformAdapter srPlatformAdapter;
