68f927399f1e382c48ad8cc7fbe6b09217c0f3e6
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-06-08 11:53:21 +0200

    This commit marks release 14.2.11

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 allprojects {
     group = "net.skinsrestorer"
-    version = "14.2.11-SNAPSHOT"
+    version = "14.2.11"
     description = "Ability to restore/change skins on servers! (Offline and Online Mode)"
 }
 
