2b1e24d3a38b733d92b39b8906a1898775cc13df
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-10-09 19:50:38 +0200

    Add 1.20.2 mapping properly

---
+++ mappings/mc-1-20-2/build.gradle.kts
@@ -0,0 +1,14 @@
+tasks {
+    remap {
+        version.set("1.20.2")
+    }
+}
+
+dependencies.apply {
+    compileOnly("org.spigotmc:spigot:1.20.2-R0.1-SNAPSHOT:remapped-mojang@jar") {
+        isTransitive = false
+    }
+    compileOnly("org.spigotmc:spigot-api:1.20.2-R0.1-SNAPSHOT") {
+        isTransitive = false
+    }
+}
