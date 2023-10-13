b76a8efb96e3d8f92fcbd26411ae97441003d7f0
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-10-13 08:47:30 +0200

    Remove dependency on jansi
    This allows us to have less natives!

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -14,8 +14,6 @@ dependencies {
     implementation("net.kyori:adventure-text-serializer-plain:4.14.0")
     api("net.kyori:adventure-text-minimessage:4.14.0")
 
-    implementation("org.fusesource.jansi:jansi:2.4.0")
-
     api("com.github.SkinsRestorer:ConfigMe:beefdbdf7e")
     api("ch.jalu:injector:1.0")
 
