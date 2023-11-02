63b3b007e977822e96b8c5502a4fdccdda0fbf73
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-11-02 20:02:14 +0100

    Use sponge manually because their build tool slows down the build

--- sponge/build.gradle.kts
+++ sponge/build.gradle.kts
@@ -1,10 +1,3 @@
-import org.spongepowered.gradle.plugin.config.PluginLoaders
-import org.spongepowered.plugin.metadata.model.PluginDependency
-
-plugins {
-    id("org.spongepowered.gradle.plugin") version "2.2.0"
-}
-
 dependencies {
     compileOnly(projects.skinsrestorerBuildData)
     implementation(projects.skinsrestorerApi)
@@ -16,36 +9,6 @@ dependencies {
 
     implementation("org.bstats:bstats-sponge:3.0.2")
     compileOnly("com.mojang:authlib:2.0.27")
-}
 
-sponge {
-    apiVersion("8.1.0")
-    license("GNU GENERAL PUBLIC LICENSE Version 3")
-    loader {
-        name(PluginLoaders.JAVA_PLAIN)
-        version("1.0")
-    }
-    injectRepositories(false)
-    plugin("skinsrestorer") {
-        displayName("SkinsRestorer")
-        entrypoint("net.skinsrestorer.sponge.SRSpongeBootstrap")
-        description(rootProject.description)
-        links {
-            homepage("https://skinsrestorer.net")
-            source("https://github.com/SkinsRestorer/SkinsRestorerX")
-            issues("https://github.com/SkinsRestorer/SkinsRestorerX/issues")
-        }
-        contributor("knat") {
-        }
-        contributor("AlexProgrammerDE") {
-        }
-        contributor("Blackfire62") {
-        }
-        contributor("McLive") {
-        }
-        dependency("spongeapi") {
-            loadOrder(PluginDependency.LoadOrder.AFTER)
-            optional(false)
-        }
-    }
+    compileOnly("org.spongepowered:spongeapi:8.1.0")
 }
