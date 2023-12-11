677e96774238960b1bd2261f7c0c7c7c6f2fc4f2
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2023-12-11 18:38:12 +0100

    This commit marks release 15.0.4
    Signed-off-by: AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "net.skinsrestorer"
-    version = "15.0.4-SNAPSHOT"
+    version = "15.0.4"
     description = "Ability to restore/change skins on servers!"
 }
 
