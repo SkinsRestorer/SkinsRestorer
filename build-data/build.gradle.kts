042800093e07f9a300c69f3d2004c9dde15fd840
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-01-18 16:48:37 +0100

    Get ci name and build number into build

--- build-data/build.gradle.kts
+++ build-data/build.gradle.kts
@@ -18,12 +18,28 @@ sourceSets {
                 property("commit", indraGit.commit()?.name ?: "unknown")
                 property("branch", indraGit.branch()?.name ?: "unknown")
                 property("build_time", SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date()))
-                property("ci_name", System.getenv("CI_NAME") ?: "local")
-                property("ci_build_number", System.getenv("CI_BUILD_NUMBER") ?: "local")
+                property("ci_name", getCIName())
+                property("ci_build_number", getBuildNumber())
 
                 val sharedResources = rootDir.resolve("shared").resolve("src").resolve("main").resolve("resources")
                 property("locales", sharedResources.resolve("locales").list()?.joinToString("|"))
             }
         }
     }
+}
+
+fun getCIName(): String {
+    val githubActions = System.getenv("GITHUB_ACTIONS")
+    val jenkinsUrl = System.getenv("JENKINS_URL")
+    if (githubActions != null && githubActions == "true") {
+        return "github-actions"
+    } else if (jenkinsUrl != null) {
+        return "jenkins"
+    }
+
+    return "local"
+}
+
+fun getBuildNumber(): String {
+    return System.getenv("BUILD_NUMBER") ?: System.getenv("GITHUB_RUN_NUMBER") ?: "local"
 }
