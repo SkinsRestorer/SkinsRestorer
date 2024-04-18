b3e1487e7877e85a5aa2065a5709c97b41590cee
xknat <kevin.natte@gmail.com>
2024-04-18 23:56:12 +0200

    This commit marks release 15.0.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "net.skinsrestorer"
-    version = "15.0.10-SNAPSHOT"
+    version = "15.0.10"
     description = "Ability to restore/change skins on servers!"
 }
 
