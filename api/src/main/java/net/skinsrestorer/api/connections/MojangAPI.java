ec55068ee40e139971933fce8a05e657a0d657eb
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-04-21 21:11:02 +0200

    Improve javadoc

--- api/src/main/java/net/skinsrestorer/api/connections/MojangAPI.java
+++ api/src/main/java/net/skinsrestorer/api/connections/MojangAPI.java
@@ -37,9 +37,6 @@ public interface MojangAPI {
     /**
      * Get skin property by player name, this method will return empty if the player is not premium.
      * It may return a hardcoded skin value, for example for "Steve" or "Alex".
-     * It does, in theory, a name to uuid to profile request.
-     * But internally it is faster than calling {@link #getUUID(String)} and {@link #getProfile(UUID)} separately.
-     * That is because we use ashcon.app, and we can directly get the profile and uuid from the name.
      *
      * @param nameOrUniqueId Can be a premium player username or unique id (dashed or non-dashed)
      * @return Skin or empty if the player is not premium
