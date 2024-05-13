323220a39e56795b7c95044a63a533f5f389bb96
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-05-13 17:35:22 +0200

    Update test dependencies

--- buildSrc/src/main/kotlin/sr.core-dependencies.gradle.kts
+++ buildSrc/src/main/kotlin/sr.core-dependencies.gradle.kts
@@ -8,8 +8,8 @@ dependencies {
     implementation("org.jetbrains:annotations:24.1.0")
     compileOnly("com.github.spotbugs:spotbugs-annotations:4.8.5")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
-    testFixturesApi("org.junit.jupiter:junit-jupiter-api:5.10.2")
-    testFixturesApi("org.mockito:mockito-core:5.11.0")
-    testFixturesApi("org.mockito:mockito-junit-jupiter:5.11.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.0-M1")
+    testFixturesApi("org.junit.jupiter:junit-jupiter-api:5.11.0-M1")
+    testFixturesApi("org.mockito:mockito-core:5.12.0")
+    testFixturesApi("org.mockito:mockito-junit-jupiter:5.12.0")
 }
