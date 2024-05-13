c62d7907fe664b10eb15d43dc37eb6587d24c3c6
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-05-13 17:32:20 +0200

    Merge test dependencies into fixtures

--- buildSrc/src/main/kotlin/sr.core-dependencies.gradle.kts
+++ buildSrc/src/main/kotlin/sr.core-dependencies.gradle.kts
@@ -8,12 +8,8 @@ dependencies {
     implementation("org.jetbrains:annotations:24.1.0")
     compileOnly("com.github.spotbugs:spotbugs-annotations:4.8.5")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
-    testImplementation("org.mockito:mockito-core:5.11.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
-
-    testFixturesImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
-    testFixturesImplementation("org.mockito:mockito-core:5.11.0")
-    testFixturesImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
+    testFixturesApi("org.junit.jupiter:junit-jupiter-api:5.10.2")
+    testFixturesApi("org.mockito:mockito-core:5.11.0")
+    testFixturesApi("org.mockito:mockito-junit-jupiter:5.11.0")
 }
