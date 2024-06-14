28d71df955b8d2aa834cf9ad85ab2dcf922de142
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-06-14 08:19:59 +0200

    Add 1.21 mappings (#1636)
    * Add 1.21 mappings
    
    * Fix build

--- bukkit/src/main/java/net/skinsrestorer/bukkit/utils/MappingManager.java
+++ bukkit/src/main/java/net/skinsrestorer/bukkit/utils/MappingManager.java
@@ -37,7 +37,8 @@ public class MappingManager {
             new Mapping1_20(),
             new Mapping1_20_2(),
             new Mapping1_20_4(),
-            new Mapping1_20_5()
+            new Mapping1_20_5(),
+            new Mapping1_21()
     );
 
     public static Optional<IMapping> getMapping(Server server) {
