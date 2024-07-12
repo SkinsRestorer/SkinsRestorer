147d7b6dc36cefd947948edb8f400d44de547cc2
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-07-12 20:53:19 +0200

    Add comments

--- shared/src/main/java/net/skinsrestorer/shared/config/GUIConfig.java
+++ shared/src/main/java/net/skinsrestorer/shared/config/GUIConfig.java
@@ -17,6 +17,7 @@
  */
 package net.skinsrestorer.shared.config;
 
+import ch.jalu.configme.Comment;
 import ch.jalu.configme.SettingsHolder;
 import ch.jalu.configme.configurationdata.CommentsConfiguration;
 import ch.jalu.configme.properties.Property;
@@ -27,17 +28,29 @@ import static ch.jalu.configme.properties.PropertyInitializer.newListProperty;
 import static ch.jalu.configme.properties.PropertyInitializer.newProperty;
 
 public class GUIConfig implements SettingsHolder {
+    @Comment("Whether custom skins are enabled in the /skins GUI")
     public static final Property<Boolean> CUSTOM_GUI_ENABLED = newProperty("gui.custom.enabled", true);
+    @Comment("Order of custom skins relative to the other skin types")
     public static final Property<Integer> CUSTOM_GUI_INDEX = newProperty("gui.custom.index", 0);
+    @Comment("Whether only specific custom skins are allowed in the /skins GUI")
     public static final Property<Boolean> CUSTOM_GUI_ONLY_LIST = newProperty("gui.custom.onlyShowList", false);
+    @Comment("Specific custom skins to show in the /skins GUI")
     public static final Property<List<String>> CUSTOM_GUI_LIST = newListProperty("gui.custom.list", "xknat", "pistonmaster");
+    @Comment("Whether player skins are enabled in the /skins GUI")
     public static final Property<Boolean> PLAYERS_GUI_ENABLED = newProperty("gui.players.enabled", false);
+    @Comment("Order of player skins relative to the other skin types")
     public static final Property<Integer> PLAYERS_GUI_INDEX = newProperty("gui.players.index", 1);
+    @Comment("Whether only specific player skins are allowed in the /skins GUI")
     public static final Property<Boolean> PLAYERS_GUI_ONLY_LIST = newProperty("gui.players.onlyShowList", false);
+    @Comment("Specific player skins to show in the /skins GUI")
     public static final Property<List<String>> PLAYERS_GUI_LIST = newListProperty("gui.players.list", "7dcfc130-344a-4719-9fbe-3176bc2075c6", "b1ae0778-4817-436c-96a3-a72c67cda060");
+    @Comment("Whether recommended skins are enabled in the /skins GUI")
     public static final Property<Boolean> RECOMMENDATIONS_GUI_ENABLED = newProperty("gui.recommendations.enabled", true);
+    @Comment("Order of recommended skins relative to the other skin types")
     public static final Property<Integer> RECOMMENDATIONS_GUI_INDEX = newProperty("gui.recommendations.index", 2);
+    @Comment("Whether only specific recommended skins are allowed in the /skins GUI")
     public static final Property<Boolean> RECOMMENDATIONS_GUI_ONLY_LIST = newProperty("gui.recommendations.onlyShowList", false);
+    @Comment("Specific recommended skins to show in the /skins GUI")
     public static final Property<List<String>> RECOMMENDATIONS_GUI_LIST = newListProperty("gui.recommendations.list", "vampire", "space-suit");
 
     @Override
@@ -45,6 +58,15 @@ public class GUIConfig implements SettingsHolder {
         conf.setComment("gui",
                 "\n",
                 "Control what skins appear in the /skins GUI"
+        );
+        conf.setComment("gui.custom",
+                "Control custom skins in the /skins GUI"
+        );
+        conf.setComment("gui.players",
+                "Control player skins in the /skins GUI"
+        );
+        conf.setComment("gui.recommendations",
+                "Control recommended skins in the /skins GUI"
         );
     }
 }
