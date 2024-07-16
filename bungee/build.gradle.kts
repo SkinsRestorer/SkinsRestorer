0bce61d9c1f34accabc938e7382fdfc7e26ce6ab
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-07-16 19:15:50 +0200

    Fix hover and click events

--- bungee/build.gradle.kts
+++ bungee/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     implementation(projects.multiver.bungee.propertynew)
     testImplementation(testFixtures(projects.skinsrestorerShared))
 
-    implementation("net.kyori:adventure-platform-bungeecord:4.3.3")
+    implementation(libs.adventure.bungeecord)
 
     compileOnly("net.md-5:bungeecord-api:1.20-R0.2") {
         isTransitive = false
