140be564ddbafd45e8b4e7b199773420a1e4f902
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-07-22 21:27:28 +0200

    Fix jvmdowngrader (#1716)
    * WIP fix jvmdowngrader unit tests
    
    * Fix some issues with downgrading

--- buildSrc/src/main/kotlin/sr.platform-logic.gradle.kts
+++ buildSrc/src/main/kotlin/sr.platform-logic.gradle.kts
@@ -1,14 +1,30 @@
+import xyz.wagyourtail.jvmdg.gradle.task.ShadeJar
+
 plugins {
     `java-library`
+    id("xyz.wagyourtail.jvmdowngrader")
     id("sr.shadow-logic")
 }
 
-tasks.shadowJar {
+val downgradePlatform = tasks.register<ShadeJar>("downgradePlatform") {
+    dependsOn(tasks.shadowJar)
+
+    inputFile = tasks.shadowJar.get().archiveFile
+    downgradeTo = JavaVersion.VERSION_1_8
     archiveFileName.set(
         "SkinsRestorer-${
-            project.name.substringAfter("skinsrestorer-").replaceFirstChar(kotlin.Char::titlecase)
-        }-${project.version}.jar"
+            project.name.substringAfter("skinsrestorer-").replaceFirstChar(Char::titlecase)
+        }-${project.version}-downgraded.jar"
     )
 
     destinationDirectory.set(rootProject.layout.buildDirectory.dir("libs"))
+    shadePath = { _ -> "net/skinsrestorer/shadow/jvmdowngrader" }
+}
+
+configurations.create("downgraded")
+
+artifacts {
+    add("downgraded", downgradePlatform.get().archiveFile) {
+        builtBy(downgradePlatform)
+    }
 }
--- universal/build.gradle.kts
+++ universal/build.gradle.kts
@@ -1,30 +1,20 @@
 plugins {
     java
-    id("xyz.wagyourtail.jvmdowngrader")
 }
 
 dependencies {
-    implementation(project(":skinsrestorer-bukkit", "shadow"))
-    implementation(project(":skinsrestorer-bungee", "shadow"))
-    implementation(project(":skinsrestorer-velocity", "shadow"))
+    implementation(project(":skinsrestorer-bukkit", "downgraded"))
+    implementation(project(":skinsrestorer-bungee", "downgraded"))
+    implementation(project(":skinsrestorer-velocity", "downgraded"))
 }
 
 tasks {
     jar {
-        archiveFileName = "SkinsRestorer-java17.jar"
+        archiveFileName = "SkinsRestorer.jar"
         destinationDirectory = rootProject.projectDir.resolve("build/libs")
 
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
         dependsOn(configurations.runtimeClasspath)
         from({ configurations.runtimeClasspath.get().map { zipTree(it) } })
-
-        finalizedBy(shadeDowngradedApi)
-    }
-    shadeDowngradedApi {
-        archiveFileName = "SkinsRestorer.jar"
-        destinationDirectory = rootProject.projectDir.resolve("build/libs")
-
-        downgradeTo = JavaVersion.VERSION_1_8
-        shadePath = { _ -> "net/skinsrestorer/shadow/jvmdowngrader" }
     }
 }
