62a41f46af8763b53d90db5b4596cd0f06f6d749
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-07-23 19:29:33 +0200

    Disable native parsers

---
+++ bukkit/src/main/java/org/incendo/cloud/bukkit/BukkitParsers.java
@@ -0,0 +1,13 @@
+package org.incendo.cloud.bukkit;
+
+import org.incendo.cloud.CommandManager;
+
+@SuppressWarnings("unused")
+public class BukkitParsers {
+    private BukkitParsers() {
+    }
+
+    public static <C> void register(final CommandManager<C> manager) {
+        // TODO: Make upstream allow registering no native parsers
+    }
+}
