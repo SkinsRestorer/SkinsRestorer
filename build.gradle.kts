4aa9140f8bd78bc60d3913c158b0336233fcd91a
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2024-09-22 07:38:41 +0200

    This commit marks release 15.4.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 allprojects {
     group = "net.skinsrestorer"
-    version = "15.4.3-SNAPSHOT"
+    version = "15.4.3"
     description = "Ability to restore/change skins on servers!"
 
     repositories {
