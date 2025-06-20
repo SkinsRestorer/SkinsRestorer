8526c963cd2d6df749dd4e524206e70aff3555d9
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-06-20 12:54:25 +0200

    feat: add cape section measurements

--- scissors/src/main/java/net/skinsrestorer/scissors/cape/CapeSection.java
+++ scissors/src/main/java/net/skinsrestorer/scissors/cape/CapeSection.java
@@ -19,22 +19,38 @@ package net.skinsrestorer.scissors.cape;
 
 import lombok.Getter;
 import lombok.ToString;
+import net.skinsrestorer.scissors.RectangleSection;
+import net.skinsrestorer.scissors.skin.SkinTag;
+import net.skinsrestorer.scissors.skin.SkinVariant;
+
+import java.util.Arrays;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Set;
 
 @Getter
 @ToString
 public enum CapeSection {
     // Cape
-    CAPE_TOP,
-    CAPE_BOTTOM,
-    CAPE_LEFT,
-    CAPE_FRONT,
-    CAPE_RIGHT,
-    CAPE_BACK,
+    CAPE_TOP(1, 0, 10, 1, CapeTag.CAPE, CapeTag.TOP),
+    CAPE_BOTTOM(11, 0, 10, 1, CapeTag.CAPE, CapeTag.BOTTOM),
+    CAPE_LEFT(0, 1, 1, 16, CapeTag.CAPE, CapeTag.LEFT),
+    CAPE_FRONT(1, 1, 10, 16, CapeTag.CAPE, CapeTag.FRONT),
+    CAPE_RIGHT(11, 1, 1, 16, CapeTag.CAPE, CapeTag.RIGHT),
+    CAPE_BACK(12, 1, 10, 16, CapeTag.CAPE, CapeTag.BACK),
     // Elytra
-    ELYTRA_TOP,
-    ELYTRA_BOTTOM,
-    ELYTRA_LEFT,
-    ELYTRA_BACK,
-    ELYTRA_RIGHT,
-    ELYTRA_FRONT;
+    ELYTRA_TOP(24, 0, 8, 2, CapeTag.ELYTRA, CapeTag.TOP),
+    ELYTRA_BOTTOM(32, 0, 8, 2, CapeTag.ELYTRA, CapeTag.BOTTOM),
+    ELYTRA_LEFT(22, 2, 2, 20, CapeTag.ELYTRA, CapeTag.LEFT),
+    ELYTRA_BACK(24, 2, 8, 20, CapeTag.ELYTRA, CapeTag.BACK),
+    ELYTRA_RIGHT(32, 2, 2, 20, CapeTag.ELYTRA, CapeTag.RIGHT),
+    ELYTRA_FRONT(34, 2, 8, 20, CapeTag.ELYTRA, CapeTag.FRONT);
+
+    private final RectangleSection rectangleSection;
+    private final Set<CapeTag> tags;
+
+    CapeSection(int x, int y, int width, int height, CapeTag... tags) {
+        this.rectangleSection = new RectangleSection(x, y, width, height);
+        this.tags = Set.of(tags);
+    }
 }
---
+++ scissors/src/main/java/net/skinsrestorer/scissors/cape/CapeTag.java
@@ -0,0 +1,12 @@
+package net.skinsrestorer.scissors.cape;
+
+public enum CapeTag {
+    CAPE,
+    ELYTRA,
+    TOP,
+    BOTTOM,
+    LEFT,
+    FRONT,
+    RIGHT,
+    BACK;
+}
